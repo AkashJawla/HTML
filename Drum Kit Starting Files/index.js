23af7b291728e881e9b19d94d489604afed88882
AkashJawla <akash.jawla1995@gmail.com>
2020-07-18 16:11:07 +0530

    event listners

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,6 +1,53 @@
+for (i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
 
-for (i=0;i<document.querySelectorAll(".drum").length ;i++){
-document.querySelectorAll(".drum")[i].addEventListener("click",function () {
-    alert(i);
-});
+    keypressed(this.innerHTML);
+
+    btnanimation(this.innerHTML);
+
+  });
+}
+
+document.addEventListener("keydown", function() {
+  keypressed(event.key);
+  btnanimation(event.key);
+})
+
+function keypressed(i) {
+  var audio;
+  switch (i) {
+    case "w":
+      audio = new Audio("sounds/crash.mp3");
+      break;
+    case "a":
+      audio = new Audio("sounds/kick-bass.mp3");
+      break;
+    case "s":
+      audio = new Audio("sounds/snare.mp3");
+      break;
+    case "d":
+      audio = new Audio("sounds/tom-1.mp3");
+      break;
+    case "j":
+      audio = new Audio("sounds/tom-2.mp3");
+      break;
+    case "k":
+      audio = new Audio("sounds/tom-3.mp3");
+      break;
+    case "l":
+      audio = new Audio("sounds/tom-4.mp3");
+      break;
+    default:
+      console.log("nope!!");
+  }
+  audio.play();
+}
+
+function btnanimation(btn) {
+  var btnpressed = document.querySelector("." + btn);
+  btnpressed.classList.add("pressed");
+  setTimeout(function() {
+    btnpressed.classList.remove("pressed")
+  }, 100);
+
 }
