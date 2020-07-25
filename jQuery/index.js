f4ca49522abb0e0c875060641408ed9906d51221
AkashJawla <akash.jawla1995@gmail.com>
2020-07-25 20:54:50 +0530

    jquery

--- jQuery/index.js
+++ jQuery/index.js
@@ -2,32 +2,50 @@
 
 $("h1").addClass("big-title");
 
+$("button").attr("class","big-title");
 
+$("button").attr("class","red-title");
 
+$("h1").click(function(){
+  $("button").css("color","blue");
+});
 
-  $("button").addClass("big-title");
+$(document).keydown(function(e){
+  $("h1").html($("h1").html()+e.key);
+  $("h1").css("color","purple");
+});
 
+$("h1").on("click",function(){
+  $("button").css("color","green");
+  $("h1 button").remove();
+});
+
+$(document).on("keyup",function(e){
+  $("h1").css("color","green");
+});
+
+$("button").on("click",function(){
+  $("h1").append("<button>new</button>");
+});
 
 
   $("h1").text("Bye");
 
-
-
   $("button").html("bye");
 
-  function displayTime() {
-     let date = new Date();
-     let time = date.toLocaleTimeString();
-     if($("h1").hasClass("red-title")){
-     $("h1").text(time);
-     $("h1").removeClass("red-title");
-     $("h1").addClass("green-title");
-   }
-   else{
-     $("h1").text(time);
-     $("h1").addClass("red-title");
-     $("h1").removeClass("green-title");
-   }
-  }
+  // function displayTime() {
+  //    let date = new Date();
+  //    let time = date.toLocaleTimeString();
+  //    if($("h1").hasClass("red-title")){
+  //    $("h1").text(time);
+  //    $("h1").removeClass("red-title");
+  //    $("h1").addClass("green-title");
+  //  }
+  //  else{
+  //    $("h1").text(time);
+  //    $("h1").addClass("red-title");
+  //    $("h1").removeClass("green-title");
+  //  }
+  // }
 
-  setInterval(displayTime, 1000);
+//  setInterval(displayTime, 1000);
