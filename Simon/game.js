4ee88ffa297d270e10a68b0f274fde19160f2c98
AkashJawla <akash.jawla1995@gmail.com>
2020-07-26 13:06:46 +0530

    game complete

--- Simon/game.js
+++ Simon/game.js
@@ -1,25 +1,73 @@
 var gamePattern = []
-var buttonColours = [ "red", "blue", "green", "yellow" ];
-var randomChosenColour
-var randomNumber;
+var buttonColours = ["red", "blue", "green", "yellow"];
 var userClickedPattern = [];
+var level = 0;
 
 
-function nextSequence(){
-  randomNumber = Math.floor((Math.random() * 4));
+function nextSequence() {
+  var randomNumber = Math.floor((Math.random() * 4));
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
+playSound(randomChosenColour);
+level = level + 1;
+$("#level-title").text("Level " + level);
+//console.log(gamePattern);
 }
 
-nextSequence();
-randomChosenColour = buttonColours[randomNumber];
-gamePattern.push(randomChosenColour);
-
-console.log(gamePattern);
-
-$("#"+gamePattern).fadeOut(100).fadeIn(100);
-(new Audio("sounds/" + gamePattern[0] + ".mp3")).play();
-
-$(".btn").on("click",function(){
+$(".btn").on("click", function() {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
-  console.log(userClickedPattern);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length-1);
+  //console.log(userClickedPattern);
 });
+
+function playSound(name){
+  (new Audio("sounds/" + name + ".mp3")).play();
+}
+
+function animatePress(currentColour){
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function(){
+    $("#" + currentColour).removeClass("pressed");
+  },100);
+}
+
+function gameOver(){
+  $("body").addClass("game-over");
+  setTimeout(function(){
+    $("body").removeClass("game-over");
+  },200);
+}
+
+function startOver(){
+  $("#level-title").text("Game Over, Press 'A' Key to Restart");
+  level = 0;
+  gamePattern = [];
+  userClickedPattern = [];
+}
+
+$(document).on("keydown",function(e){
+  if(level === 0 && e.key.toLowerCase() =="a"){
+    nextSequence();}
+});
+
+function checkAnswer(currentLevel){
+  if(userClickedPattern[currentLevel]==gamePattern[currentLevel]){
+    console.log("success");
+    if(gamePattern.length == currentLevel+1){
+      userClickedPattern=[];
+      setTimeout(nextSequence,1000);
+    }
+  }
+  else{
+    playSound("wrong");
+    gameOver();
+    startOver();
+    console.log("fail");
+  }
+
+
+}
