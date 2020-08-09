d7da027d8ebea16d14070735f0a568bb090ab889
AkashJawla <akash.jawla1995@gmail.com>
2020-08-09 15:41:51 +0530

    express

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/bye", function(req,res){
-  res.send("hello");
+app.get("/", function(req,res){
+  res.sendFile(__dirname + "/index.html");
 });
 
+app.post("/", function(req,res){
+  var h = Number(req.body.h);
+  var w = Number(req.body.w);
+  var result = w/(h*h);
+  res.send(result+"");
+});
 
 app.listen(3000,function(){
   console.log("server 3000 started");
